91db37e711fede49d51e18a6e7744044bde02d44
Mahendra26052006 <duggireddyuma1976@gmail.com>
2025-06-13 09:59:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="mlproject",
     version="0.0.1",
-    author="Krish",
-    author_email="krishnaik06@gmail.com",
+    author="Mahendar Reddy",
+    author_email="duggireddyuma1976@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
